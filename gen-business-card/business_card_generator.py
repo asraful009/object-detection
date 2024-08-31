440ebbecf3a080aa41f872409ee4ab585e594df7
asraful islam <asraful009@gmail.com>
2024-08-31 17:32:31 +0600

    card box location with value

--- gen-business-card/business_card_generator.py
+++ gen-business-card/business_card_generator.py
@@ -3,21 +3,23 @@
 import random
 import cv2
 import numpy as np
-from PIL import ImageDraw, ImageFont, Image, ImageChops
+from PIL import ImageDraw, ImageFont, Image
 
 from person_info import PersonInfo
 
 class BusinessCardGenerator:
-    def __init__(self, height = 320):
+    def __init__(self, height = 320, ):
         self.__ratio = 1.75
         self.__height = height
         self.__width = int(height * self.__ratio)
         self.__person_info = None
         self.__image_pil = None
+        self.__card_location = ((0, 0), (640, 640))
+        self.__rotation_val = random.randint(0, 359)
 
-
-    def generate(self, person_info: PersonInfo)-> "BusinessCardGenerator":
+    def generate(self, person_info: PersonInfo, rotation_val = 0)-> "BusinessCardGenerator":
         self.__person_info = person_info
+        self.__rotation_val = rotation_val
         rgb = (
             tuple(
                 int(self.__person_info.background_color.lstrip('#')[i:i+2], 16)
@@ -31,12 +33,12 @@ class BusinessCardGenerator:
         self.__write_text(self.__person_info.email, position=(owner_info_pos[0], owner_info_pos[1] + 76),
                           font_size=14, font_type="email")
         self.__write_text(self.__person_info.phone, position=(owner_info_pos[0], owner_info_pos[1] + 92),
-                   font_size=16)
+                          font_size=16)
         self.__draw_line(start_point=(0, self.__height - 42), end_point=(self.__width, self.__height - 42),
-                     line_color=(200, 0, 0),
-                     line_thickness=2)
+                         line_color=(200, 0, 0),
+                         line_thickness=2)
         self.__write_text(f"{self.__person_info.address}",
-                   position=(16, self.__height - 40,), font_size=20)
+                          position=(16, self.__height - 40,), font_size=20)
 
         self.__draw_image(self.__person_info.logo_path, position=(380, 72))
         self.__write_text(text=f"{self.__person_info.company}", position=(220, 212), font_size=28)
@@ -47,15 +49,16 @@ class BusinessCardGenerator:
         return self
 
     def __draw_line(self,
-                  start_point=(50, 50),
-                  end_point=(450, 450),
-                  line_color=(255, 0, 0), line_thickness=5):
+                    start_point=(50, 50),
+                    end_point=(450, 450),
+                    line_color=(255, 0, 0), line_thickness=5):
         draw = ImageDraw.Draw(self.__image_pil)
         draw.line([start_point, end_point], fill=line_color, width=line_thickness)
 
     def __draw_image(self, image_to_paste_path, position=(100, 50)):
         image_to_paste = Image.open(image_to_paste_path)
         self.__image_pil.paste(image_to_paste, position)
+
     def __image_rotation(self, rotation = 0):
         im = self.__image_pil.rotate(rotation, expand=True, resample=Image.BICUBIC)
         w, h = im.size
@@ -68,9 +71,9 @@ class BusinessCardGenerator:
 
     def __draw_background(self):
         background_image = Image.open(self.__person_info.background_image)
-        rotation_val = random.randint(0, 359)
+        w, h = background_image.size
         # print(rotation_val)
-        rotated_image_expanded = self.__image_rotation(rotation_val)
+        rotated_image_expanded = self.__image_rotation(self.__rotation_val)
         w, h = rotated_image_expanded.size
         bw, bh = background_image.size
         bw, bh = random.randint(0, bw - w -640), random.randint(0, bh - h - 640)
@@ -78,12 +81,13 @@ class BusinessCardGenerator:
         pl, pt = (320 - int(w / 2)), (320 - int(h / 2))
         background_image.paste(rotated_image_expanded,
                                (pl, pt),
-                                mask=rotated_image_expanded)
+                               mask=rotated_image_expanded)
 
         self.__image_pil = background_image
         # print(f"({pl}, {pt}) ({w}, {h})")
-        draw = ImageDraw.Draw(self.__image_pil)
-        draw.rectangle([(pl, pt), (pl+w, pt+h)], outline=(100, 100, 100), width=5)
+        # draw = ImageDraw.Draw(self.__image_pil)
+        # draw.rectangle([(pl, pt), (pl+w, pt+h)], outline=(100, 100, 100), width=5)
+        self.__card_location = ((pl, pt), (pl+w, pt+h))
 
     def __write_text(self, text, position=(20, 20),
                      font_size=40, text_color=(0, 0, 0),
@@ -96,3 +100,6 @@ class BusinessCardGenerator:
     def get_cv2_image(self):
         image_cv = cv2.cvtColor(np.array(self.__image_pil), cv2.COLOR_RGB2BGR)
         return image_cv
+
+    def get_card_location(self):
+        return self.__card_location
--- gen-business-card/main.py
+++ gen-business-card/main.py
@@ -2,10 +2,9 @@
 from person_info import PersonInfo
 from business_card_generator import BusinessCardGenerator
 import os
-import json
 import cv2
 import random
-import uuid
+import numpy as np
 
 dir_path = f"{os.path.dirname(os.path.realpath(__file__))}/../asserts"
 
@@ -23,20 +22,26 @@ def main():
     bc_gen = BusinessCardGenerator(320)
     index = 0
     for image in images:
-        p = PersonInfo(dir_path,
-                       f"{image}",
-                       f"{bg_images[random.randint(0, len(bg_images) - 1)]}",
-                       "bn_BD")
-        # print(json.dumps(p.to_dict()))
-        cv_image = bc_gen.generate(p).get_cv2_image()
-        id = uuid.uuid4()
-        cv2.imwrite(f"{dir_path}/dataset/images/train/{index}.jpg", cv_image)
-        index += 1
-        # cv2.imshow("image", cv_image)
-        # cv2.waitKey(0)
-        # cv2.destroyAllWindows()
-
-        # exit(0)
+        rotations = np.random.randint(0, 359, size=10)
+        for rotation in rotations:
+            ix_str = f"{index}_{rotation}"
+            p = PersonInfo(dir_path,
+                           f"{image}",
+                           f"{bg_images[random.randint(0, len(bg_images) - 1)]}",
+                           "bn_BD")
+            # print(json.dumps(p.to_dict()))
+            bc_gen.generate(p, rotation)
+            cv_image = bc_gen.get_cv2_image()
+            card_location = bc_gen.get_card_location()
+            w, h = card_location[1][0]/640, card_location[1][1]/640
+            cv2.imwrite(f"{dir_path}/dataset/images/train/{ix_str}.jpg", cv_image)
+            with open(f'{dir_path}/dataset/labels/train/{ix_str}.txt', 'w') as file:
+                file.write(f"0 0.5 0.5 {w} {h}")
+            index += 1
+            # cv2.imshow("image", cv_image)
+            # cv2.waitKey(0)
+            # cv2.destroyAllWindows()
+            # exit(0)
         if index > 0:
             break
 
