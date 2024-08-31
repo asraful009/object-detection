3ef0c6fa3d89e11a593da1f599d62dd633378d45
asraful islam <asraful009@gmail.com>
2024-08-31 21:48:13 +0600

    train fail for val is not normallize and out of GPU MEN

--- train_model/main.py
+++ train_model/main.py
@@ -7,31 +7,31 @@ dir_path = f"{os.path.dirname(os.path.realpath(__file__))}/../asserts"
 
 def main():
     model = YOLO('yolov8n.pt')
-    model.train(data=f'{dir_path}/dataset/dataset.yaml', epochs=1, imgsz=640, batch=100, device="cuda")
+    model.train(data=f'{dir_path}/dataset/dataset.yaml', epochs=1, imgsz=640, batch=100)
     model.save(f'{dir_path}/dataset/custom_model.pt')
     # Load the image where you want to detect objects
-    image_path = f'{dir_path}/dataset/images/train/1.jpg'  # Replace with the path to your image
-    image = cv2.imread(image_path)
+    # image_path = f'{dir_path}/dataset/images/train/1.jpg'  # Replace with the path to your image
+    # image = cv2.imread(image_path)
 
     # Perform object detection
-    results = model.predict(image)
-    print(results)
+    # results = model.predict(image)
+    # print(results)
     # Visualize the results
-    for result in results:
-        for box in result.boxes:
-            x1, y1, x2, y2 = map(int, box.xyxy[0])  # Get bounding box coordinates
-            label = result.names[box.cls]  # Get the label/class name
-            confidence = box.conf[0]  # Get the confidence score
-
-            # Draw the bounding box on the image
-            cv2.rectangle(image, (x1, y1), (x2, y2), (0, 255, 0), 2)
-            text = f'{label} ({confidence:.2f})'
-            cv2.putText(image, text, (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 1.5, (255, 255, 255), 5)
+    # for result in results:
+    #     for box in result.boxes:
+    #         x1, y1, x2, y2 = map(int, box.xyxy[0])  # Get bounding box coordinates
+    #         label = result.names[box.cls]  # Get the label/class name
+    #         confidence = box.conf[0]  # Get the confidence score
+    #
+    #         # Draw the bounding box on the image
+    #         cv2.rectangle(image, (x1, y1), (x2, y2), (0, 255, 0), 2)
+    #         text = f'{label} ({confidence:.2f})'
+    #         cv2.putText(image, text, (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 1.5, (255, 255, 255), 5)
 
     # Display the image with detected objects
-    cv2.imshow('YOLOv8 Object Detection', image)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+    # cv2.imshow('YOLOv8 Object Detection', image)
+    # cv2.waitKey(0)
+    # cv2.destroyAllWindows()
 
 if __name__ == "__main__":
     main()
